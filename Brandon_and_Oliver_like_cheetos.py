d0987be488a024afa58b5dac8989741c9c433e1e
Catastrophic Robotics 23-24 <primary@thekingsburys.com>
2023-10-22 17:37:24 -0400

    Add Brandon and Oliver gen 1 prototype test Cheeto Delivery Bot

---
+++ Brandon_and_Oliver_like_cheetos.py
@@ -0,0 +1,28 @@
+from pybricks.parameters import Stop, Port, Direction
+from base_robot_australian_pest_controller import BaseRobot
+cheeto_delivery_robot = BaseRobot()
+# cheeto_delivery_robot.db.settings(straight_speed=700)
+# cheeto_delivery_robot.db.straight(600,Stop.NONE) 
+# cheeto_delivery_robot.db.curve(150,90,Stop.NONE)
+# cheeto_delivery_robot.db.straight(900,Stop.NONE)
+# cheeto_delivery_robot.db.curve(300,90,Stop.NONE)
+# cheeto_delivery_robot.db.curve(250,-90)
+
+# Augmented Reality
+cheeto_delivery_robot.db.settings(straight_speed=500)
+cheeto_delivery_robot.db.straight(600,Stop.NONE) 
+cheeto_delivery_robot.db.curve(150,90,Stop.NONE)
+cheeto_delivery_robot.db.straight(600,Stop.NONE)
+cheeto_delivery_robot.db.turn(-60,Stop.NONE)
+cheeto_delivery_robot.db.straight(145)
+cheeto_delivery_robot.db.turn(90)
+cheeto_delivery_robot.db.straight(350)
+cheeto_delivery_robot.db.turn(-45)
+cheeto_delivery_robot.db.straight(250)
+cheeto_delivery_robot.db.turn(100)
+cheeto_delivery_robot.db.straight(250)
+cheeto_delivery_robot.db.turn(100)
+cheeto_delivery_robot.db.straight(50)
+cheeto_delivery_robot.db.straight(-200)
+cheeto_delivery_robot.db.turn(-90)
+cheeto_delivery_robot.db.straight(200)
---
+++ base_robot_australian_pest_controller.py
@@ -0,0 +1,19 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase, GyroDriveBase
+from pybricks.tools import wait, StopWatch
+
+class BaseRobot:
+
+    def __init__(self):
+        self.hub = PrimeHub()
+        self.leftMotor = Motor(Port.A, Direction.COUNTERCLOCKWISE)
+        self.rightMotor = Motor(Port.E, Direction.CLOCKWISE)
+        self.topLeftMotor = Motor(Port.B, Direction.COUNTERCLOCKWISE)
+        self.topRightMotor = Motor(Port.F, Direction.CLOCKWISE)
+        self.wheelDiameter = 56
+        self.axleTrack = 121.5
+        self.db = GyroDriveBase(self.leftMotor, self.rightMotor,
+            self.wheelDiameter, self.axleTrack)
+
