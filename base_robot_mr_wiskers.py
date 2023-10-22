ca72de8a83d524134353a3bff0333d117c2f4bf4
Catastrophic Robotics 23-24 <primary@thekingsburys.com>
2023-10-22 17:38:11 -0400

    Add Elin and Natalia gen 1 prototype Mr. Wiskers

---
+++ base_robot_mr_wiskers.py
@@ -0,0 +1,18 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase, GyroDriveBase
+from pybricks.tools import wait, StopWatch
+
+class BaseRobot:
+
+    def __init__(self):
+        self.hub = PrimeHub()
+        self.leftMotor = Motor(Port.B, Direction.COUNTERCLOCKWISE)
+        self.rightMotor = Motor(Port.A, Direction.CLOCKWISE)
+        self.frontMoter = Motor(Port.C)
+        self.wheelDiameter = 87
+        self.axleTrack = 113
+        self.db = GyroDriveBase(self.leftMotor, self.rightMotor,
+            self.wheelDiameter, self.axleTrack)
+
---
+++ mr_wiskers.py
@@ -0,0 +1,30 @@
+from pybricks.parameters import Stop, Port, Direction
+from base_robot_mr_wiskers import BaseRobot
+mr = BaseRobot()
+
+# Elin's
+mr.frontMoter.run_target(300,-70)
+mr.db.straight(512)
+mr.db.turn(-30)
+mr.db.straight(88)
+mr.frontMoter.run_time(300,1000)
+mr.frontMoter.run_target(300,5)
+mr.db.turn(50)
+mr.db.straight(-109)
+mr.frontMoter.run_target(300,-70)
+mr.db.curve(300,68)
+mr.db.settings(straight_speed=600)
+mr.db.straight(1000,Stop.NONE)
+mr.db.curve(300,90,Stop.NONE)
+mr.db.straight(500)
+
+# Natalia's
+#
+# mr.db.straight(512)
+# mr.db.turn(-30)
+# mr.db.straight(88)
+# mr.frontMoter.run_time(300,1000)
+# mr.frontMoter.run_target(300,45)
+# mr.db.straight(-100)
+# mr.db.turn(90)
+# mr.db.straight(100)
