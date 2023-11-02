b5fe17dec549195843e7c40af1725ca09806d414
Catastrophic Robotics 23-24 <primary@thekingsburys.com>
2023-11-01 20:14:05 -0400

    Add latest runs to main game and tweaks

--- fll_2023_24.py
+++ fll_2023_24.py
@@ -1,12 +1,14 @@
 from pybricks.tools import hub_menu
 
 # Choose a letter.
-selected = hub_menu("A", "B", "C")
+selected = hub_menu("A", "B", "C", "D")
 
 # Based on the selection, run a program.
 if selected == "A":
-    import run_example_1
+    import run_westside_lift
 elif selected == "B":
-    import run_example_2
+    import run_east_side_push
 elif selected == "C":
-    import run_example_2
+    import run_emily
+elif selected == "D":
+    import run_rolly_banna_elin
--- run_westside_lift.py
+++ run_westside_lift.py
@@ -1,22 +1,44 @@
 from base_robot_mr_wiskers_2 import BaseRobot
+from pybricks.parameters import Stop
 
 # Oliver's West Side Lift Missions
 # Does NOT yet return to launch
 #
 Puffball = BaseRobot()
+Puffball.db.settings(800, 600, 200, 400)
+
+# Drive To 3D Movie
 Puffball.frontMotor.run_target(270,75)
 Puffball.db.curve(350,-90)
-Puffball.frontMotor.run_target(-270,-5)
-Puffball.db.turn(180)
-Puffball.db.curve(300,-90)
-Puffball.frontMotor.run_target(270,75)
-Puffball.db.turn(-35)
-Puffball.db.straight(30)
-Puffball.frontMotor.run_time(-270,1000)
-Puffball.db.turn(90)
-Puffball.frontMotor.run_target(270,75)
-Puffball.db.curve(400,30)
-Puffball.db.straight(250)
-Puffball.db.turn(-45)
-Puffball.frontMotor.run_target(270,0)
 
+# Do 3D Movie
+Puffball.frontMotor.run_target(-270,-5)
+Puffball.db.turn(185)
+Puffball.frontMotor.run_target(270,75)
+
+# Drive to Scene Changer
+Puffball.db.curve(350,-80)
+Puffball.db.turn(-53)
+Puffball.db.straight(80)
+
+# Change Scene
+Puffball.frontMotor.run_time(-270,1000)
+Puffball.frontMotor.run_target(270,75)
+
+# Drive to Immersive Experience
+Puffball.db.turn(90)
+Puffball.db.curve(400,40)
+Puffball.db.straight(210)
+Puffball.db.turn(-90)
+Puffball.db.straight(10)
+
+# Do Immersive Experience
+Puffball.frontMotor.run_time(-90,1200)
+Puffball.frontMotor.run_target(270,75)
+
+# Back to East Home
+Puffball.db.straight(-10,Stop.NONE)
+Puffball.db.turn(90)
+Puffball.db.straight(550,Stop.NONE)
+Puffball.db.curve(450,90,Stop.NONE)
+Puffball.db.straight(400)
