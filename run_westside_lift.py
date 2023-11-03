aac41c8d4deb342b199b7c359885a8f637806227
Catastrophic Robotics 23-24 <primary@thekingsburys.com>
2023-11-02 20:33:08 -0400

    Update runs for black wheel

--- run_westside_lift.py
+++ run_westside_lift.py
@@ -5,11 +5,11 @@ from pybricks.parameters import Stop
 # Does NOT yet return to launch
 #
 Puffball = BaseRobot()
-Puffball.db.settings(800, 600, 200, 400)
+Puffball.db.settings(600, 600, 200, 300)
 
 # Drive To 3D Movie
 Puffball.frontMotor.run_target(270,75)
-Puffball.db.curve(350,-90)
+Puffball.db.curve(345,-95)
 
 # Do 3D Movie
 Puffball.frontMotor.run_target(-270,-5)
@@ -17,20 +17,21 @@ Puffball.db.turn(185)
 Puffball.frontMotor.run_target(270,75)
 
 # Drive to Scene Changer
-Puffball.db.curve(350,-80)
+Puffball.db.curve(370,-80)
 Puffball.db.turn(-53)
-Puffball.db.straight(80)
+Puffball.db.straight(70)
 
 # Change Scene
 Puffball.frontMotor.run_time(-270,1000)
 Puffball.frontMotor.run_target(270,75)
 
 # Drive to Immersive Experience
+Puffball.db.straight(-55)
 Puffball.db.turn(90)
-Puffball.db.curve(400,40)
+Puffball.db.curve(340,45)
 Puffball.db.straight(210)
 Puffball.db.turn(-90)
-Puffball.db.straight(10)
+Puffball.db.straight(15)
 
 # Do Immersive Experience
 Puffball.frontMotor.run_time(-90,1200)
@@ -40,5 +41,5 @@ Puffball.frontMotor.run_target(270,75)
 Puffball.db.straight(-10,Stop.NONE)
 Puffball.db.turn(90)
 Puffball.db.straight(550,Stop.NONE)
-Puffball.db.curve(450,90,Stop.NONE)
+Puffball.db.curve(400,90,Stop.NONE)
 Puffball.db.straight(400)
