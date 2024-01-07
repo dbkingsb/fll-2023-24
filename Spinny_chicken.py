e55a8e8b9b9580d0da4e278f9fecddf321a49da4
Catastrophic Robotics 23-24 <primary@thekingsburys.com>
2024-01-07 15:43:40 -0500

    Max, Natalia, Oliver create Spinny_chicken run

---
+++ Spinny_chicken.py
@@ -0,0 +1,10 @@
+from base_robot_mr_wiskers_2 import BaseRobot
+
+
+
+
+puffmonster= BaseRobot()
+# puffmonster.db.straight(100)
+# puffmonster.db.turn(-43)
+# puffmonster.db.straight(400)
+puffmonster.frontMotor.run_time(500,5000)
